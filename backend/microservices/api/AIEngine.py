106f4bf5cbea16c9516cf7998c4f327016899d14
Chaitanya Ashtekar <chaitanya.ashtekar17@vit.edu>
2022-03-18 21:06:12 +0530

    Class for executing ML model

--- backend/microservices/api/AIEngine.py
+++ backend/microservices/api/AIEngine.py
@@ -1,25 +1,21 @@
-import pickle
+
 import sklearn
 import spacy
-import pathlib
 
 class AIEngine:
 
     def __init__(self,sentTokens,nlpModel):
         self.nlpModel = nlpModel
         self.sentTokens = sentTokens
-        self.path = pathlib.Path(__file__).parent.resolve()
         self.probabilityOrder = ['Hate Speech','Neutral','Offensive Language']
         self.probabilityMap = dict()
 
 
-    def predictUsingVotingClassifier(self):
+    def predictUsingVotingClassifier(self,model):
         try:
-            pathToModel = str(self.path) + '/' + 'Voting.pkl'
-            vcModel = pickle.load(open(pathToModel,'rb'))
             vectors=self.nlpModel(self.sentTokens).vector.reshape(1,-1)
-            output = vcModel.predict(vectors)
-            probabilities = vcModel.predict_proba(vectors)
+            output = model.predict(vectors)
+            probabilities = model.predict_proba(vectors)
             probabilityList = list(probabilities.flatten())
             for prob,key in zip(probabilityList,self.probabilityOrder):
                 self.probabilityMap[key] = prob
